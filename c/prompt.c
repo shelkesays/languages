1e3f16b36235f947610f9dd8268e7183bc5856fa
Rahul Shelke <srahul07@gmail.com>
2016-07-22 00:59:14 +0530

    Updating name

--- c/prompt.c
+++ c/prompt.c
@@ -1,30 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* If we are compiling on Windows compile these functions */
+#ifdef _WIN32
+#include <string.h>
+
+/* Fake readline function */
+char* readline(char* prompt) {
+	fputs(prompt, stdout);
+	fgets(buffer, 2048, stdin);
+	char* cpy = malloc(strlen(buffer)+1);
+	strcpy(cpy, buffer);
+	cpy[strlen(cpy)-1] = '\0';
+	return cpy;
+}
+
+/* Fake add history function */
+void add_history(char* unused) {}
+
+/* otherwise include the editline headers */
+#else
 #include <editline/readline.h>
 #include <editline/history.h>
-
-/* Declare a buffer for user input of size 2048 */
+#endif
 
 int main(int argc, char** argv) {
 	
 	/* Print version and Exit Information  */
-	puts("Lispy Version 0.0.0.0.1");
+	puts("Ark Version 0.0.0.0.1");
 	puts("Press Ctrl+c to Exit\n");
+	puts("This is a general purpose language, developed for educational purpose.");
+	puts("Do not use it in production! :)\n");
 
 	/* In never ending loop */
 	while (1) {
 		
-		/* Output our prompt and get input */
-		char* input = readline("lispy> ");
-		
-		/* Add input to history */
+		/* Now in either case readline will be correctly defined  */
+		char* input = readline("ark> ");
 		add_history(input);
 
-		/* Echo input back to user */
-		printf("No you're a %s\n", input);
-
-		/* Fre retrieved input */
+		printf("%s\n", input);
 		free(input);
 	}
 	
