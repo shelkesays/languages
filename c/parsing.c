7205e43a364bd32d0f0cefa912c4fc28dba079a2
Rahul Shelke <srahul07@gmail.com>
2016-07-23 01:26:05 +0530

    Adding evaluations

--- c/parsing.c
+++ c/parsing.c
@@ -24,6 +24,42 @@ void add_history(char* unused) {}
 #include <editline/history.h>
 #endif
 
+/* Use operator string to see which operation to perform */
+long eval_op(long x, char* op, long y) {
+	if (strcmp(op, "+") == 0 || strcmp(op, "add") == 0) { return x + y; }
+	if (strcmp(op, "-") == 0 || strcmp(op, "sub") == 0) { return x - y; }
+	if (strcmp(op, "*") == 0 || strcmp(op, "mul") == 0) { return x * y; }
+	if (strcmp(op, "/") == 0 || strcmp(op, "div") == 0) { return x / y; } 
+	if (strcmp(op, "%") == 0 || strcmp(op, "mod") == 0) { return x % y; }
+	if (strcmp(op, "^") == 0 || strcmp(op, "raise") == 0) { return x ^ y; }
+	if (strcmp(op, "min") == 0) { return x < y ? x : y; }
+	if (strcmp(op, "max") == 0) { return x > y ? x : y; }
+	return 0;
+}
+
+long eval(mpc_ast_t* t) {
+
+	/* Tagged as number return it directly. */
+	if(strstr(t->tag, "number")) {
+		return atoi(t->contents);
+	}
+	
+	/* The operator is always second child. */
+	char* op = t->children[1]->contents;
+
+	/* We store the third child in `x` */
+	long x = eval(t->children[2]);
+
+	/* Iterate the remaining children and combining. */
+	int i = 3;
+	while(strstr(t->children[i]->tag, "expr")) {
+		x = eval_op(x, op, eval(t->children[i]));
+		i++;
+	}
+
+	return x;
+}
+
 int main(int argc, char** argv) {
 	
 	/* Create some parser */
@@ -36,7 +72,7 @@ int main(int argc, char** argv) {
 	mpca_lang(MPCA_LANG_DEFAULT,
 		"										\
 	   		number		:	/-?[0-9]+[.]?[0-9]*/ ;				\
-	   		operator	:	'+' | '-' | '*' | '/' | '%' | \"add\" | \"sub\" | \"mul\" | \"div\";		\
+	   		operator	:	'+' | '-' | '*' | '/' | '%' | \"add\" | \"sub\" | \"mul\" | \"div\" | \"mod\" | \"raise\" | \"min\" | \"max\" ;		\
 	   		expr		:	<number> | '(' <operator> <expr>+ ')' ;		\
 	   		ark		:	/^/ <operator> <expr>+ /$/ ;			\
 		",
@@ -45,6 +81,7 @@ int main(int argc, char** argv) {
 	/* Print version and Exit Information  */
 	puts("Ark Version 0.0.0.0.2");
 	puts("Press Ctrl+c to Exit\n");
+	puts("You can also type `exit` OR `q` OR `quit` to Exit\n");
 
 	/* In never ending loop */
 	while (1) {
@@ -53,11 +90,16 @@ int main(int argc, char** argv) {
 		char* input = readline("ark> ");
 		add_history(input);
 
+		if (strcmp(input, "exit") == 0 || strcmp(input, "q") == 0 || strcmp(input, "quit") == 0) {
+			exit(0);
+		}
+
 		/* Attempt to parse the user input */
 		mpc_result_t r;
 		if(mpc_parse("<stdin>", input, Ark, &r)) {
-			/* on success Print the LAST */
-			mpc_ast_print(r.output);
+			
+			long result = eval(r.output);
+			printf("%li\n", result);
 			mpc_ast_delete(r.output);
 		} else {
 			/* Otherwise Print the Error */
